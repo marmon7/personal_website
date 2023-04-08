9d3066cb050615a8cf0f98e7cbcd0637346e362c
Brandon <bbarnett15@my.bcit.ca>
2023-04-07 23:03:54 -0700

    finished createcoursearray() function

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,51 @@
-let courselist = [
-    {code: "ACIT 1620", name: "Fundemental Web Technologies" },
-    {code: "ACIT 1515", name: "Scripting for IT"},
-    {code: "ACIT 1630", name: "Database Systems"}
-]
-
-do {
-    answer = prompt("Course Number?(4 digit code)")
-    if (answer.length != 4) {
-        answer = 'a'
+let courselist = []
+function createCourseArray() {
+    let codes = document.querySelectorAll("li>a")
+    let dates = document.querySelectorAll("li>p:nth-child(2)")
+    for (let idx = 0; idx < codes.length; idx++) {
+        let course = {code: codes[idx].textContent, date: dates[idx].textContent}
+        courselist.push(course)
     }
-} while (isNaN(answer))
-
-let message = ''
-for (let course of courselist) {
-    if (course.code.includes(answer)) {
-        message = `Yes I am taking the course: ${course.code} - ${course.name}`
-        console.log(message)
-    }
-
 }
-if (message == '') {
-    let newcourse = {
-        code: answer,
-        name: null
+createCourseArray()
+
+function findCourse(courselist) {
+    let answer = 1
+    while (isNaN(answer) === false)  {
+        let user = prompt("Course Number?(4 digit code)")
+        if (user.length != 4) {
+            continue
+        }
+        answer = user
+        break
     }
-    courselist.push(newcourse)
-    console.log("Successfully added course")
+    
+    course_answer = "#course"+answer
+    
+    for (let course of courselist) {
+        if (course.code.includes(answer)) {
+            let select = document.querySelector(course_answer)
+            select.courselist.add("green")
+            
+        }
+    }
 }
+findCourse(courselist)
+// let message = ''
+// for (let course of courselist) {
+//     if (course.code.includes(answer)) {
+//         message = `Yes I am taking the course: ${course.code} - ${course.name}`
+//         console.log(message)
+//     }
+
+// }
+// if (message == '') {
+//     let newcourse = {
+//         code: answer,
+//         name: null
+//     }
+//     courselist.push(newcourse)
+//     console.log("Successfully added course")
+// }
 
     
